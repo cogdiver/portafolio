cfefd868c5a7676582e807145310f99bdab92282
cogdiver <valentina.arenas@xertica.com>
2023-05-11 18:13:04 -0500

    change scripts, cloudbuild and workflow to execute test

--- 005__build-workflows/scripts/up.sh
+++ 005__build-workflows/scripts/up.sh
@@ -10,7 +10,6 @@ source $env_file
 # Create Service Account
 gcloud iam service-accounts create $SERVICE_ACCOUNT
 
-
 # # Set permissions
 gcloud projects add-iam-policy-binding $PROJECT --member=serviceAccount:$SERVICE_ACCOUNT@$PROJECT.iam.gserviceaccount.com --role=roles/workflows.admin
-# gcloud builds triggers describe $TRIGGER_NAME --format="value(substitutions._CLOUD_BUILD_SERVICE_ACCOUNT)"
+
