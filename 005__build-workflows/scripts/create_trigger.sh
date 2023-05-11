0e3e4c12a61e3708c66bb8752eb3386e794ab3a7
cogdiver <valentina.arenas@xertica.com>
2023-05-11 16:18:22 -0500

    create script to configure trigger in Cloud Build

---
+++ 005__build-workflows/scripts/create_trigger.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# set login
+./scripts/set_gcp_login.sh
+
+# Load environment variables
+env_file=".env"
+source $env_file
+
+# Create trigger for github repository
+gcloud beta builds triggers create github \
+  --name=$TRIGGER_NAME \
+  --repo-name=$GITHUB_REPO_NAME \
+  --repo-owner=$GITHUB_REPO_OWNER \
+  --branch-pattern=$BRANCH_PATTERN \
+  --build-config=$BUILD_CONFIG_FILE
