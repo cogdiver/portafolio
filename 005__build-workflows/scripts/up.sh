0a929736cf9330de2f92fafd670bd7b491246df8
cogdiver <valentina.arenas@xertica.com>
2023-05-11 17:14:48 -0500

    update UP scrit

--- 005__build-workflows/scripts/up.sh
+++ 005__build-workflows/scripts/up.sh
@@ -2,3 +2,10 @@
 
 # set login
 ./scripts/set_gcp_login.sh
+
+# Load environment variables
+env_file=".env"
+source $env_file
+
+# Set permissions
+gcloud projects add-iam-policy-binding $PROJECT --member=user:$ACCOUNT --role=roles/workflows.editor
