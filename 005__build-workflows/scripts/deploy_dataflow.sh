10c0994ac263de1171d6d94e87824154c1d7a3c4
cogdiver <valentina.arenas@xertica.com>
2023-05-22 19:36:04 -0500

    add temporal script to test dataflow deployment

--- 005__build-workflows/scripts/deploy_dataflow.sh
+++ 005__build-workflows/scripts/deploy_dataflow.sh
@@ -1,21 +1,37 @@
 #!/bin/bash
 
 # set login
-./scripts/set_gcp_login.sh
+# ./scripts/set_gcp_login.sh
 
 # Load environment variables
 env_file=".env"
 source $env_file
 
 
-python dataflow_pipeline.py \
-  --runner=DataflowRunner \
-  --project=$PROJECT \
-  --region=$REGION \
-  --temp_location=<TEMP_LOCATION> \
-  --staging_location=<STAGING_LOCATION>
+# python3 template/template_count.py \
+#   --runner=DataflowRunner \
+#   --project=$PROJECT \
+#   --region=$REGION \
+#   --temp_location=gs://005__build-workflows/temp/ \
+#   # --staging_location=<STAGING_LOCATION>
 
-gcloud dataflow jobs run 005_build_worflows \
-  --gcs-location gs://$BUCKET_NAME/template/template_count.py \
+# gsutil cp template/*  gs://005__build-workflows/template/
+# gcloud dataflow jobs run 005__build_workflows \
+#   --gcs-location gs://$BUCKET_NAME/template/template_count.py \
+#   --region $REGION
+# --parameters='1,2'
+# --parameters inputFile=gs://$BUCKET_NAME/template/input.txt,output=gs://$BUCKET_NAME/output
+
+# gcloud dataflow jobs run 005__build_workflows \
+#   --gcs-location gs://dataflow-templates/latest/Word_Count \
+#   --region $REGION \
+#   --parameters inputFile=gs://$BUCKET_NAME/template/input.txt,output=gs://$BUCKET_NAME/template/output.txt
+# gsutil cp gs://005__build-workflows/template/output.txt template/
+
+
+gcloud dataflow flex-template run build-workflows \
+  --template-file-gcs-location gs://$BUCKET_NAME/template/template_count.py \
+  --temp-location gs://$BUCKET_NAME/temp/ \
   --region $REGION \
-  --parameters inputFile=gs://$BUCKET_NAME/input.txt,output=gs://$BUCKET_NAME/output
+  --parameters inputFile=gs://$BUCKET_NAME/template/input.txt,output=gs://$BUCKET_NAME/output
+  # --parameters='1,2'
