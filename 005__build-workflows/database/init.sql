a80242cd9a58909275908716aa22e6001cfffc06
cogdiver <valentina.arenas@xertica.com>
2023-06-24 22:12:23 -0500

    creeate bigquery tables to migrate

---
+++ 005__build-workflows/database/init.sql
@@ -0,0 +1,15 @@
+CREATE OR REPLACE TABLE `fine-sublime-315119.005_build_workflows.table_1`(
+  key STRING
+);
+
+CREATE OR REPLACE TABLE `fine-sublime-315119.005_build_workflows.table_2`(
+  data STRING,
+  metadatos STRING,
+  publish_time TIMESTAMP,
+  subscription_name STRING,
+  message_id STRING,
+  attributes STRING,
+  col1 STRING,
+  col2 STRING
+);
+
