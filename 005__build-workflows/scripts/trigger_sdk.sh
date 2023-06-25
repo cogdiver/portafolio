bc539db6c74f7d2196c82bd346c4e382d008b34e
cogdiver <valentina.arenas@xertica.com>
2023-06-24 22:18:35 -0500

    add file with structure to publish message from sdk

---
+++ 005__build-workflows/scripts/trigger_sdk.sh
@@ -0,0 +1,6 @@
+# Publish message on Pub/Sub
+gcloud pubsub topics publish $TOPIC_NAME --message='{"col1": "message_c1_1","col2":"message_c2_1"}'
+
+# Publish message on Cloud Run
+
+# Publish message on Cloud Functions
