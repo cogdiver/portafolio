cd2174eb993884f450f9a5790866fbdf4fe248eb
cogdiver <valentina.arenas@xertica.com>
2023-09-05 20:35:39 -0500

    update database init queries

--- 005__gcp_ci_cd/database/init.sql
+++ 005__gcp_ci_cd/database/init.sql
@@ -1,14 +1,4 @@
-CREATE OR REPLACE TABLE `<PROJECT>.<DATASET>.table_1`(
-  key STRING
-);
-
-CREATE OR REPLACE TABLE `<PROJECT>.<DATASET>.table_2`(
-  data STRING,
-  metadatos STRING,
-  publish_time TIMESTAMP,
-  subscription_name STRING,
-  message_id STRING,
-  attributes STRING,
-  col1 STRING,
-  col2 STRING
+CREATE OR REPLACE TABLE `<PROJECT>.<DATASET>.logs` (
+  id STRING,
+  message STRING
 );
