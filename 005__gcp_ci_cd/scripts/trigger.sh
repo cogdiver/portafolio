88a16fc0fc15e3e47bfd3a5f34364845f3db896e
cogdiver <valentina.arenas@xertica.com>
2023-09-06 08:07:49 -0500

    init cli trigger

--- 005__gcp_ci_cd/scripts/trigger.sh
+++ 005__gcp_ci_cd/scripts/trigger.sh
@@ -1,6 +1,26 @@
-# Publish message on Pub/Sub
-gcloud pubsub topics publish $TOPIC_NAME --message='{"col1": "message_c1_1","col2":"message_c2_1"}'
+#!/bin/bash
 
-# Publish message on Cloud Run
+# -----------------------------------------------------------------------------------------
+# Documentation here...
+# -----------------------------------------------------------------------------------------
+
+# set login
+./scripts/gcp_login.sh
+
+# Set environment variables using the .env file
+env_file=".env"
+source $env_file
+
+
+# # Publish message on Pub/Sub
+# gcloud pubsub topics publish $TOPIC_NAME --message='{"id": "uuidgen","message":"07:41"}'
+
+# # Publish message on Cloud Run
+# # 'https://image-project-005-55nsgsicwq-uc.a.run.app/v1/logs/' \
+# curl -X 'POST' \
+#   'http://localhost:8080/v1/logs/' \
+#   -H 'accept: application/json' \
+#   -H 'Content-Type: application/json' \
+#   -d '{"message": "7:52"}'
 
 # Publish message on Cloud Functions
