03a4e182039bc52fd66690b43be981effcd648c5
cogdiver <valentina.arenas@xertica.com>
2023-09-20 08:50:51 -0500

    add sdk trigger

--- 005__gcp_ci_cd/scripts/trigger.sh
+++ 005__gcp_ci_cd/scripts/trigger.sh
@@ -1,30 +1,56 @@
 #!/bin/bash
 
-# -----------------------------------------------------------------------------------------
-# Documentation here...
-# -----------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------
+# This script demonstrates publishing messages to different Google Cloud
+# services such as Pub/Sub, Cloud Run, and Cloud Functions based on the
+# specified message type.
+# --------------------------------------------------------------------------
 
-# set login
-./scripts/gcp_login.sh
+# Load environment variables from the .env file
+source .env
 
-# Set environment variables using the .env file
-env_file=".env"
-source $env_file
+# Function to publish a message to Pub/Sub
+publish_to_pubsub() {
+    local message="$1"
+    gcloud pubsub topics publish $TOPIC_NAME --message='{"id": "'$(uuidgen)'", "message": "'$message'"}'
+}
 
+# Function to publish a message to Cloud Run
+publish_to_cloud_run() {
+  local message="$1"
+  curl -X POST "$CLOUD_RUN_URL/v1/logs/" \
+    -H 'accept: application/json' \
+    -H 'Content-Type: application/json' \
+    -d "{\"message\": \"$message\"}"
+}
 
-# Publish message on Pub/Sub
-gcloud pubsub topics publish $TOPIC_NAME --message='{"id": "uuidgen","message":"0400 trigger-sdk-to-pub/sub"}'
+# Function to publish a message to Cloud Functions
+publish_to_cloud_functions() {
+  local message="$1"
+  local type="$2"
+  curl -X POST $FUNCTION_URL \
+    -H 'accept: application/json' \
+    -H 'Content-Type: application/json' \
+    -d '{"message": "'$message'", "type": "'$type'"}'
+}
 
-# Publish message on Cloud Run
-curl -X 'POST' \
-  'https://image-project-005-55nsgsicwq-uc.a.run.app/v1/logs/' \
-  -H 'accept: application/json' \
-  -H 'Content-Type: application/json' \
-  -d '{"message": "0400 trigeer-sdk-to-cloud-run"}'
+# Check if the required number of arguments are provided
+if [ $# -lt 2 ]; then
+  echo "Usage: $0 <service> <message>"
+  exit 1
+fi
 
-# Publish message on Cloud Functions
-curl -X 'POST' \
-  'https://us-central1-fine-sublime-315119.cloudfunctions.net/function_project_005' \
-  -H 'accept: application/json' \
-  -H 'Content-Type: application/json' \
-  -d '0400 trigger-sdk-to-functions'
+service="$1"
+message="$2"
+type="$3"
+
+# Execute the corresponding action based on the specified type
+case "$service" in
+  "pubsub") publish_to_pubsub "$message" ;;
+  "cloud_run") publish_to_cloud_run "$message" ;;
+  "cloud_functions") publish_to_cloud_functions "$message" "$type" ;;
+  *)
+    echo "Invalid type. Supported services are: pubsub, cloud_run, cloud_functions."
+    exit 1
+    ;;
+esac
