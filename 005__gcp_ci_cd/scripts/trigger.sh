9174d826b48be7a71c7a65934e699a7d72ad8101
cogdiver <valentina.arenas@xertica.com>
2023-09-20 04:01:50 -0500

    update to general test

--- 005__gcp_ci_cd/scripts/trigger.sh
+++ 005__gcp_ci_cd/scripts/trigger.sh
@@ -12,20 +12,19 @@ env_file=".env"
 source $env_file
 
 
-# # Publish message on Pub/Sub
-# gcloud pubsub topics publish $TOPIC_NAME --message='{"id": "uuidgen","message":"07:41"}'
+# Publish message on Pub/Sub
+gcloud pubsub topics publish $TOPIC_NAME --message='{"id": "uuidgen","message":"0400 trigger-sdk-to-pub/sub"}'
 
-# # Publish message on Cloud Run
-# # 'https://image-project-005-55nsgsicwq-uc.a.run.app/v1/logs/' \
-# curl -X 'POST' \
-#   'http://localhost:8080/v1/logs/' \
-#   -H 'accept: application/json' \
-#   -H 'Content-Type: application/json' \
-#   -d '{"message": "7:52"}'
+# Publish message on Cloud Run
+curl -X 'POST' \
+  'https://image-project-005-55nsgsicwq-uc.a.run.app/v1/logs/' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{"message": "0400 trigeer-sdk-to-cloud-run"}'
 
-# # Publish message on Cloud Functions
-# curl -X 'POST' \
-#   'http://localhost:8080/v1/logs/' \
-#   -H 'accept: application/json' \
-#   -H 'Content-Type: application/json' \
-#   -d '7:52'
+# Publish message on Cloud Functions
+curl -X 'POST' \
+  'https://us-central1-fine-sublime-315119.cloudfunctions.net/function_project_005' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '0400 trigger-sdk-to-functions'
